6bca0b58e7529ef4597507696171fe7c6887cf5c
Dante Pizzini <pizzini@wisc.edu>
2021-04-17 11:23:32 -0600

    updated Employee model

--- P1TicketReimbursement/src/main/java/com/revature/models/Employee.java
+++ P1TicketReimbursement/src/main/java/com/revature/models/Employee.java
@@ -1,5 +1,51 @@
 package com.revature.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Employee {
+	
+	private int employeeID;
+	private String firstName;
+	private String username;
+	private String pass;
+	private List<Ticket> myTickets = new ArrayList<Ticket>();
+	
+	public Employee(int employeeID, String firstName, String username, String pass) {
+		this.employeeID = employeeID;
+		this.firstName = firstName;
+		this.username = username;
+		this.setPass(pass);
+	}
+
+	public List<Ticket> getMyTickets() {
+		return myTickets;
+	}
+
+	public void setMyTickets(List<Ticket> myTickets) {
+		this.myTickets = myTickets;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public int getEmployeeID() {
+		return employeeID;
+	}
+	
+	
 
 }
