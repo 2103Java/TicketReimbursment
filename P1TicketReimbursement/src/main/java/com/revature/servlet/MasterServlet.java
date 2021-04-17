f68ceef3308c902934a6803d3ae48842bd024fe5
Jerdmann94 <62105705+Jerdmann94@users.noreply.github.com>
2021-04-17 11:55:41 -0500

    test

--- P1TicketReimbursement/src/main/java/com/revature/servlet/MasterServlet.java
+++ P1TicketReimbursement/src/main/java/com/revature/servlet/MasterServlet.java
@@ -3,16 +3,35 @@ package com.revature.servlet;
 import java.io.IOException;
 import java.sql.SQLException;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class MasterServlet extends HttpServlet {
-	
+
+	public static RequestHelper rh = new RequestHelper();
+
+
 	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException{
 		
 		System.out.println("Server is up");
+
+		
+		HttpServletRequest  request;
+        HttpServletResponse response;
+        
+        if (!(req instanceof HttpServletRequest &&
+                resp instanceof HttpServletResponse)) {
+            throw new ServletException("non-HTTP request or response");
+        }
+
+        request = (HttpServletRequest) req;
+        response = (HttpServletResponse) resp;
+       
+		rh.process(request,response);
+
 		
 	}
 
--- P1TicketReimbursement/src/main/java/com/revature/servlet/RequestHelper.java
+++ P1TicketReimbursement/src/main/java/com/revature/servlet/RequestHelper.java
@@ -1,5 +1,13 @@
 package com.revature.servlet;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class RequestHelper  {
+
+	public void process(HttpServletRequest req, HttpServletResponse resp) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
