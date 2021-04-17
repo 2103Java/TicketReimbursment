b093a94576c57df114a7f19fa23bd3abf916981f
Dante Pizzini <pizzini@wisc.edu>
2021-04-17 10:48:03 -0600

    updated create, select, update behaviors in TicketDao, select behavior in EmployeeDao

--- P1TicketReimbursement/src/main/java/com/revature/dao/TicketDao.java
+++ P1TicketReimbursement/src/main/java/com/revature/dao/TicketDao.java
@@ -1,5 +1,27 @@
 package com.revature.dao;
 
+import java.sql.Connection;
+import java.util.List;
+
+import com.revature.models.Employee;
+import com.revature.models.Ticket;
+
 public interface TicketDao {
+	
+	//CREATE
+	public boolean insertNewTicket();
+	
+	//SELECT
+	public List<Ticket> retrieveAllTickets(Connection conn);
+	public List<Ticket> retrieveTicketByApproval(boolean approval, Connection conn);
+	public List<Ticket> retrieveTicketByEmployee(Employee E, Connection conn);
+	public List<Ticket> retrieveTicketByType(String type, Connection conn);
+	
+	//UPDATE
+	public boolean approveTicket(Ticket t, Connection conn);
+	public boolean rejectTicket(Ticket t, Connection conn);
+	
+	//DELETE
+	
 
 }
