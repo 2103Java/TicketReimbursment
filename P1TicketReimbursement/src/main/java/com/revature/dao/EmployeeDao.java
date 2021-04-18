374823640555dc13e84f423ea4664d60418eadbc
Dante Pizzini <pizzini@wisc.edu>
2021-04-18 15:10:37 -0600

    update EmployeeDao and EmployeeDaoImpl with sql statement, verify enum is being inserted and read correctly

--- P1TicketReimbursement/src/main/java/com/revature/dao/EmployeeDao.java
+++ P1TicketReimbursement/src/main/java/com/revature/dao/EmployeeDao.java
@@ -12,6 +12,8 @@ public interface EmployeeDao {
 	
 	//SELECT
 	public Employee findEmployee(Employee E, Connection conn);
+	public Employee findEmployeeByID(int id, Connection conn);
+	public Employee findEmployeeByTicketID(int id, Connection conn);
 	
 	//UPDATE
 	public boolean updatePassword(Employee E, Connection conn);
--- P1TicketReimbursement/src/main/java/com/revature/dao/EmployeeDaoImpl.java
+++ P1TicketReimbursement/src/main/java/com/revature/dao/EmployeeDaoImpl.java
@@ -1,6 +1,9 @@
 package com.revature.dao;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import com.revature.models.Employee;
 
@@ -8,37 +11,118 @@ public class EmployeeDaoImpl implements EmployeeDao {
 
 	@Override
 	public boolean insertNewEmployee(Employee E, Connection conn) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			Statement stmnt = conn.createStatement();
+			String sql = "INSERT INTO tktemployees values ('"+ E.getUsername()+"','"+E.getPass()+"','"+E.getEmployment()+"',nextval('tktemployees_user_id_seq'))";
+
+			stmnt.executeUpdate(sql);
+		} catch (SQLException e) {
+			return false;
+		}
+		return true;
 	}
 
 	@Override
 	public Employee findEmployee(Employee E, Connection conn) {
-		// TODO Auto-generated method stub
-		return null;
+		Employee employee = null;
+		try {
+			Statement stmnt = conn.createStatement();
+			String sql = "SELECT * from tktemployees where user_id = " + E.getEmployeeID();
+
+			ResultSet rs = stmnt.executeQuery(sql);
+			
+			while(rs.next()) {
+				String username = rs.getString("username");
+				String pass = rs.getString("password");
+				Employee.CATEGORY employment = Employee.CATEGORY.valueOf(rs.getString("type"));
+				int user_id = rs.getInt("user_id");
+				
+				employee = new Employee(username, pass, employment, user_id);
+			
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return employee;
 	}
 
 	@Override
+	public Employee findEmployeeByID(int id, Connection conn) {
+		Employee employee = null;
+		try {
+			Statement stmnt = conn.createStatement();
+			String sql = "SELECT * from tktemployees where user_id = " + id;
+
+			ResultSet rs = stmnt.executeQuery(sql);
+			
+			while(rs.next()) {
+				String username = rs.getString("username");
+				String pass = rs.getString("password");
+				Employee.CATEGORY employment = Employee.CATEGORY.valueOf(rs.getString("type"));
+				int user_id = rs.getInt("user_id");
+				
+				employee = new Employee(username, pass, employment, user_id);
+			
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return employee;
+	}
+
+	@Override
+	public Employee findEmployeeByTicketID(int id, Connection conn) {
+		Employee employee = null;
+		try {
+			Statement stmnt = conn.createStatement();
+			String select = "(SELECT user_id from tickets where ticket_id = "+id+")";
+			String sql = "SELECT * from tktemployees where user_id = " + select;
+
+			ResultSet rs = stmnt.executeQuery(sql);
+			
+			while(rs.next()) {
+				String username = rs.getString("username");
+				String pass = rs.getString("password");
+				Employee.CATEGORY employment = Employee.CATEGORY.valueOf(rs.getString("type"));
+				int user_id = rs.getInt("user_id");
+				
+				employee = new Employee(username, pass, employment, user_id);
+			
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return employee;
+	}
+	
+	
+	@Override
 	public boolean updatePassword(Employee E, Connection conn) {
-		// TODO Auto-generated method stub
+		// STRETCH GOAL
 		return false;
 	}
 
 	@Override
 	public boolean updatePersonalInfo(Employee E, Connection conn) {
-		// TODO Auto-generated method stub
+		// STRETCH GOAL
 		return false;
 	}
 
 	@Override
 	public boolean deleteEmployee(Employee E, Connection conn) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			Statement stmnt = conn.createStatement();
+			String sql = "DELETE FROM tktemployees WHERE username = '" + E.getUsername() +"'";
+
+			stmnt.executeUpdate(sql);
+		} catch (SQLException e) {
+			return false;
+		}
+		return true;
 	}
 
-	public Employee retrieveTicketByID(int id, Connection conn) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
+
+	
 
 }
