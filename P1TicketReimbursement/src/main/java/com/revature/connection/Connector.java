bfdff0a96c21243e49039c5e97d468fd67f3dcb3
Dante Pizzini <pizzini@wisc.edu>
2021-04-19 20:35:04 -0600

    I don't believe I actually changed anything in this file

--- P1TicketReimbursement/src/main/java/com/revature/connection/Connector.java
+++ P1TicketReimbursement/src/main/java/com/revature/connection/Connector.java
@@ -5,32 +5,27 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Connector {
-	
-		
-		
+
 	//
-		private static final String URL = "jdbc:postgresql://database-1.csd4vggmmxv0.us-east-2.rds.amazonaws.com";
-		private static final String USERNAME = "postgres";
-		private static final String PASSWORD = "password";
-		
-		private static Connection conn;
-		
-		public static Connection setUpConnection() 
-		{
-			
-			try {
-				conn = DriverManager.getConnection(URL,USERNAME,PASSWORD);
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			return conn;
-				
+	private static final String URL = "jdbc:postgresql://database-1.csd4vggmmxv0.us-east-2.rds.amazonaws.com:5432/postgres";
+	private static final String USERNAME = "postgres";
+	private static final String PASSWORD = "password";
+
+	private static Connection conn;
+
+	public static Connection setUpConnection() {
+
+		try {
+			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
+		return conn;
 	}
 
+}
 
 //
 //endpoint
